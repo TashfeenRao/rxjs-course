16b800c6a206194e16a27958f4201dd398fde6c6
tashfeen rao <tashfeendev@gmail.com>
2021-06-11 00:16:18 +0500

    async values added

--- lesson2.ts
+++ lesson2.ts
@@ -3,6 +3,15 @@ import { Observable } from "rxjs";
 const onservable$ = new Observable<string>((subscriber) => {
   console.log("observable is executed");
   subscriber.next("Tahfeen");
+  subscriber.next("Rao"); // these values emit instently after subscribing
+
+  setTimeout(() => {
+    subscriber.next("Is learning the rxjs");
+  }, 1000);
+  setTimeout(() => {
+    subscriber.next("I am enjoying to learn");
+  }, 3000);
+  // These are async values the result came back after some time the it returns the value
 });
 
 console.log("before subscription");
--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ define("lesson2", ["require", "exports", "rxjs"], function (require, exports, rx
     var onservable$ = new rxjs_1.Observable(function (subscriber) {
         console.log("observable is executed");
         subscriber.next("Tahfeen");
+        subscriber.next("Rao");
+        setTimeout(function () {
+            subscriber.next("Is learning the rxjs");
+        }, 1000);
+        setTimeout(function () {
+            subscriber.next("I am enjoying to learn");
+        }, 3000);
     });
     console.log("before subscription");
     onservable$.subscribe(function (value) { return console.log(value); });
