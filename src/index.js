f0ca4019e7f7c1dbfd6e0b7cddb938b819958c9f
tashfeen rao <tashfeendev@gmail.com>
2021-06-16 00:18:51 +0500

    implemented the interval function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 define("timer", ["require", "exports", "rxjs"], function (require, exports, rxjs_1) {
     "use strict";
     exports.__esModule = true;
-    var sub2 = rxjs_1.timer(2000).subscribe({
-        next: function (value) { return console.log(value); },
-        complete: function () { return console.log("completed!"); }
-    });
+    var sub2 = rxjs_1.timer(2000).subscribe({});
     setTimeout(function () {
         sub2.unsubscribe();
     }, 1000);
@@ -18,11 +15,29 @@ define("timer", ["require", "exports", "rxjs"], function (require, exports, rxjs
             clearTimeout(timeId);
         };
     });
-    var sub1 = ourTimer$.subscribe({
-        next: function (value) { return console.log(value); },
-        complete: function () { return console.log("Completed!"); }
-    });
+    var sub1 = ourTimer$.subscribe({});
     setTimeout(function () {
         sub1.unsubscribe();
     }, 1000);
+    var sub3 = rxjs_1.interval(1000).subscribe({});
+    setTimeout(function () {
+        sub3.unsubscribe();
+    }, 5000);
+    var ourInterval$ = new rxjs_1.Observable(function (subscriber) {
+        var counter = 0;
+        var timeId = setInterval(function () {
+            console.log("memory leak?");
+            subscriber.next(++counter);
+        }, 1000);
+        return function () {
+            clearInterval(timeId);
+        };
+    });
+    var sub4 = ourInterval$.subscribe({
+        next: function (value) { return console.log(value); }
+    });
+    setTimeout(function () {
+        sub4.unsubscribe();
+        console.log("unsubscribed");
+    }, 5000);
 });
--- timer.ts
+++ timer.ts
@@ -1,8 +1,8 @@
-import { Observable, timer } from "rxjs";
+import { interval, Observable, timer } from "rxjs";
 
 const sub2 = timer(2000).subscribe({
-  next: (value) => console.log(value),
-  complete: () => console.log("completed!"),
+  //next: (value) => console.log(value),
+  //complete: () => console.log("completed!"),
 });
 
 setTimeout(() => {
@@ -22,10 +22,43 @@ const ourTimer$ = new Observable<number>((subscriber) => {
 });
 
 const sub1 = ourTimer$.subscribe({
-  next: (value) => console.log(value),
-  complete: () => console.log("Completed!"),
+  //next: (value) => console.log(value),
+  //complete: () => //console.log("Completed!"),
 });
 
 setTimeout(() => {
   sub1.unsubscribe();
 }, 1000);
+
+// Interval
+
+const sub3 = interval(1000).subscribe({
+  // next: (value) => console.log(value),
+  // complete: () => console.log("completed!"),
+});
+
+setTimeout(() => {
+  sub3.unsubscribe();
+  //console.log("unsubscribed");
+}, 5000);
+
+const ourInterval$ = new Observable<number>((subscriber) => {
+  let counter: number = 0;
+  const timeId = setInterval(() => {
+    console.log("memory leak?");
+    subscriber.next(++counter);
+  }, 1000);
+  return () => {
+    clearInterval(timeId);
+  };
+  // it will prevent from memory leak
+});
+
+const sub4 = ourInterval$.subscribe({
+  next: (value) => console.log(value),
+});
+
+setTimeout(() => {
+  sub4.unsubscribe();
+  console.log("unsubscribed");
+}, 5000);
